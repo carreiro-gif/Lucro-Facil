71c102e9accd32a1202357b59507bdf8fef292d7
carreiro-gif <espacocarreiro@gmail.com>
2025-12-02 11:48:58 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../App";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+// 🔥 Importa Firebase principal (firebase.ts)
+import "./firebase/firebase.ts";
+
+// 🔥 Importa o CRUD de ingredientes
+import "./firebase/firebase-ingredients.ts";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
