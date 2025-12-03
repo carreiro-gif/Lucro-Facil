0dd182928e8e892bdbd9d279e26367c92e07b4e4
carreiro-gif <espacocarreiro@gmail.com>
2025-12-03 12:27:12 -0300

    Update Dna.tsx

--- src/pages/Dna.tsx
+++ src/pages/Dna.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useApp } from '../context/AppContext';
+import { useApp } from "../../context/AppContext";
 import { Info, Settings2, TrendingDown, TrendingUp, Minus } from 'lucide-react';
 
 const Dna: React.FC = () => {
