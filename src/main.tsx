6ef7144eed92356abbf979911bf09cb4e9264227
carreiro-gif <espacocarreiro@gmail.com>
2025-12-03 12:45:51 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../App";
 
-// 🔥 Importa apenas o arquivo que realmente EXISTE
+// ✔️ Caminho correto para App.tsx dentro de /src
+import App from "./App";
+
+// 🔥 Importa o Firebase ingredients
 import "./firebase/firebase-ingredients.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
